2403463e106bcd05632bf4eb37d493562ab6ae9b
godcong <jumbycc@163.com>
2025-06-24 18:15:11 +0800

    refactor(database): rename package from drivers to database
    - Move database.go from contrib/database/drivers/ to contrib/database/
    - Update package name from "drivers" to "database"
    - Update package documentation to reflect new package name

--- contrib/database/database.go
+++ contrib/database/database.go
@@ -2,8 +2,8 @@
  * Copyright (c) 2024 OrigAdmin. All rights reserved.
  */
 
-// Package drivers is the database client wrapper
-package drivers
+// Package database implements the functions, types, and interfaces for the module.
+package database
 
 import (
 	"database/sql"
